ce1554d787ee17c8b3a55abdd94f6518d42e7489
tonny <anthonny14gabriell@gmail.com>
2023-09-06 16:18:17 -0400

    styling header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,12 @@ import Link from "next/link"
 
 const Header = () => {
   return (
-    <header className="flex justify-between px-16 py-4 bg-emerald-600">
+    <header className="flex justify-center md:justify-between px-16 py-4 bg-emerald-600">
         <nav className="flex gap-4">
             <Link href={'/'}>Home</Link>
             <Link href={'/movies'}>Movies</Link>
         </nav>
-        <div>
+        <div className="hidden md:block">
             something here
         </div>
     </header>
